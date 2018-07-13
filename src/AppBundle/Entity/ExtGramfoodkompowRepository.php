eb54ac806a61dc80c16e7d3c74f3ba5b39a90801
Mariusz Ludwikowski <ludwikowski.mariusz@gmail.com>
2018-07-13 06:59:55 +0200

    zmiana techniczna

--- src/AppBundle/Entity/ExtGramfoodkompowRepository.php
+++ src/AppBundle/Entity/ExtGramfoodkompowRepository.php
@@ -5,17 +5,29 @@ namespace AppBundle\Entity;
 use Doctrine\ORM\EntityRepository;
 use Doctrine\ORM\Query\ResultSetMapping;
 
+
 /**
  * ExtGramfoodkompowRepository
  *
  */
-class ExtGramfoodkompowRepository {
+class ExtGramfoodkompowRepository extends EntityRepository {
 
      /**
      * @return string
      */
     public function findToExt($idKpl)
     {
-        return $idKpl;
+        //$em = $this->entityManager->createQueryBuilder();
+        $em = $this->getEntityManager()->createQueryBuilder();
+       // $em = $this->getDoctrine()->entityManager->createQueryBuilder();
+        $em
+        ->select('d', 'e')
+        ->from('AppBundle\Entity\Gramfoodklembowdok', 'd')
+        ->leftJoin('AppBundle\Entity\ExtGramfoodkompow', 'e', \Doctrine\ORM\Query\Expr\Join::WITH, 'd.id = e.idkpl')
+        ->where('d.typ like \'KPL\' ')
+        ->orderBy('d.id', 'DESC');
+        
+        return $em->getQuery()->getResult();
+
     }
 }
